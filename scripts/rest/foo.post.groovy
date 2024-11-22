15b127adb9a012eacedf1e642934a46791326d3b
admin admin <evaladmin@example.com>
2024-11-22 15:57:40 -0500

    "User admin wrote content /scripts/rest/foo.post.groovy"

--- scripts/rest/foo.post.groovy
+++ scripts/rest/foo.post.groovy
@@ -1,14 +1,18 @@
-def parts = [:]
 println "========================================================================================================================================"
 
-request.reader.text.split('&').each { param ->
-def (key, value) = param.split('=').collect { URLDecoder.decode(it, 'UTF-8') }
-parts[key] = value
-}
+def parts = request.getParts()
+
+// reader.text.split('&').each { param ->
+// def (key, value) = param.split('=').collect { URLDecoder.decode(it, 'UTF-8') }
+// parts[key] = value
+// }
+
+println ""+parts
 // Access individual fields
-parts.each { key, value ->
-println "parts: ${key}: ${value}"
-}
+//parts.each { key, value ->
+//println "parts: ${key}: ${value}"
+//}
+
 println ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 params.each { key, value ->
